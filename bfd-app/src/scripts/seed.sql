b628be4ddb53f932847d97a82f02a4247b967ac2
Jaron <jayisaac6706@gmail.com>
2026-05-20 21:33:19 -0700

    Began adding products to seed.sql

---
+++ bfd-app/src/scripts/seed.sql
@@ -0,0 +1,46 @@
+INSERT INTO products (productName, productDesc, productType, productPrice, image_url) VALUES
+-- Mice Products
+('Luxuper M-A - Compact Mouse',
+'Ambidextrous, compacr design emphasizing comfort and simplicity. Engineered with an aluminum scroll wheel, dedicated DPI button for up to 3,000 DPI & an additional programmable button for multi-use. 125-1,000 Hz polling rate.',
+'Mice',
+17.99,
+'images/'),
+
+('Luxuper M-AW - Wireless Compact Mouse',
+'Wireless version of the Luxuper M-A. All features included, with up to 20 hours battery life. Bluetooth-compatible, also usable with included Type-C wire and/or USB-A 2.4GHz dongle.',
+'Mice',
+29.99,
+'images/'),
+
+('Luxuper M-C - Lightweight Ergonomic Mouse',
+'Lightweight compact mouse designed for efficiency, with silent click switches. Engineered with an aluminum scroll wheel, dedicated DPI button for up to 4,000 DPI & an additional programmable button for multi-use. True 1,000 Hz polling rate.',
+'Mice',
+35.99,
+'images/'
+),
+('Luxuper M-CW - Wireless Lightweight Ergonomic Mouse',
+'Wireless version of the Luxuper M-C. All features included, with up to 40 hours battery life. Bluetooth-compatible, also usable with included Type-C wire and/or USB-A dongle. Charging dock included.',
+'Mice',
+45.99,
+'images/'),
+
+('Luxuper M-Cx Pro - Ergonomic Mouse',
+'Designed with quality in mind. An upgrade from the compact version, with an advanced vertical design, full aluminum frame. Features a dedicated DPI button for up to 12,000 DPI, 6 programmable buttons, and a customizable RGB indicator. True 4000-8000Hz polling rate.',
+'Mice',
+69.99,
+'images/'),
+
+('Luxuper M-CWx Pro - Wireless Ergonomic Mouse',
+'Compared to the Luxuper M-Cx Pro model, the M-CWx Pro includes all features presented with an additional charging dock, customizable side-scroll wheel, and underglow LED lighting.',
+'Mice',
+110.99,
+'images/'),
+
+
+
+-- Keyboard Products
+
+
+-- Audio Products
+
+-- Camera Products
