8082819be21ad765c8441347b4dcdef129894355
KJ <kjmiclea@gmail.com>
2026-05-26 18:09:44 -0700

    Added getAllProducts() to service.js

--- src/services/service.js
+++ src/services/service.js
@@ -9,3 +9,22 @@ const pool = mysql.createConnection({
     database: process.env.DATABASE_,
     port: process.env.PORT_
 }).promise();
+
+const fetchProducts = async () => {
+    const sql = "SELECT * FROM products ORDER BY id";
+    const products = await pool.query(sql);
+    console.log(products);
+    return products[0];
+}
+
+export const getAllProducts = () => ({
+        "productCount": fetchProducts.length,
+        "productData": fetchProducts.map(el => ({
+                id: el.id,
+                name: el.productName,
+                desc: el.productDesc,
+                type: el.productType,
+                price: el.producePrice,
+                imgUrl: el.image_url
+            }))
+    });
