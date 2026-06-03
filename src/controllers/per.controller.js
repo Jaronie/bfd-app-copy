b5d29305a0810c6f05f39d42da32ea6b5ea54cc8
KJ <kjmiclea@gmail.com>
2026-06-03 14:17:34 -0700

    Added statusCheck and debugged getById in per.controller

--- src/controllers/per.controller.js
+++ src/controllers/per.controller.js
@@ -1,5 +1,13 @@
 import { getAllProducts, getProductById} from '../services/service.js';
 
+export const statusCheck = (req, res) => {
+    res.status(200).json({
+        "ok": true,
+        "service": "Luxuper High-End Peripherals API",
+        "time": new Date().toUTCString()
+    });
+}
+
 export const getProducts = (req, res) => {
     const productArray = getAllProducts();
     return res.status(200).json(productArray);
@@ -7,19 +15,19 @@ export const getProducts = (req, res) => {
 
 export const getById = (req, res) => {
     const id = Number(req.params.id);
-
     const product = getProductById(id);
 
-    if(!product){
+    if (product) {
         return res.status(404).json({
             message: `Product with ID ${id} not found.`,
             data: null
         });
-        }
-
-        res.status(200).json({
+    }
+    else {
+        return res.status(200).json({
             message: 'Product found',
             data: product
         });
-    };
+    }
+};
 
