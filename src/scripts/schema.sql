0245a066682f3db2ac8426728b3e1d76ab6d9f9c
KJ <kjmiclea@gmail.com>
2026-06-04 14:08:23 -0700

    Increased description size for item in schema.sql

--- src/scripts/schema.sql
+++ src/scripts/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     productName VARCHAR(255),
-    productDesc VARCHAR(255),
+    productDesc VARCHAR(1000),
     productType VARCHAR(50),
     producePrice DECIMAL(10, 2),
     image_url VARCHAR(255)
