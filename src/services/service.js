4d59731bb59362745d725165ae4b7205eca898f0
Jaron <jayisaac6706@gmail.com>
2026-06-04 18:29:30 -0700

    removed filtering from services

--- src/services/service.js
+++ src/services/service.js
@@ -1,34 +1,23 @@
 import pool from "../scripts/db.js";
 
-const fetchProducts = async (filters = {}) => {
-    const conditions = [];
-    const params = [];
-
-    if (filters.name) {
-        conditions.push("productName LIKE ?");
-        params.push(`%${filters.name}%`);
-    }
-
-    if (filters.category) {
-        conditions.push("productType = ?");
-        params.push(filters.category);
-    }
-
-    if (filters.minPrice) {
-        conditions.push("producePrice >= ?");
-        params.push(Number(filters.minPrice));
-    }
-
-    if (filters.maxPrice) {
-        conditions.push("producePrice <= ?");
-        params.push(Number(filters.maxPrice));
-    }
-
-    const where = conditions.length ? ` WHERE ${conditions.join(" AND ")}` : "";
-    const sql = `SELECT * FROM products${where} ORDER BY id`;
+const fetchProducts = async () => {
+    const sql = "SELECT * FROM products ORDER BY id";
 
     try {
-        const [rows] = await pool.query(sql, params);
+        const [rows] = await pool.query(sql);
+        return rows;
+    } catch (err) {
+        console.log("-- Error retrieving data from database. --");
+        console.log(err);
+        console.log("-- End of SQL error. --")
+        return null;
+    }    
+}
+const fetchProductById = async () => {
+    const sql = "SELECT * FROM products ORDER BY id = ?";
+
+    try {
+        const [rows] = await pool.query(sql, [id]);
         return rows;
     } catch (err) {
         console.log("-- Error retrieving data from database. --");
@@ -38,22 +27,8 @@ const fetchProducts = async (filters = {}) => {
     }    
 }
 
-const fetchProductById = async (id) => {
-    const sql = "SELECT * FROM products WHERE id = ?";
-
-    try {
-        const [rows] = await pool.query(sql, [id]);
-        return rows[0];
-    } catch (err) {
-        console.log("-- Error retrieving data from database. --");
-        console.log(err);
-        console.log("-- End of SQL error. --")
-        return null;
-    }    
-}
-
-export const getAllProducts = async (filters = {}) => {
-    return await fetchProducts(filters);
+export const getAllProducts = async () => {
+    return await fetchProducts();
 };
 
 export const getProductById = async (id) => {
