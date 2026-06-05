6444e970011283a53bb7f376604f275a11f34b5c
Jaron <jayisaac6706@gmail.com>
2026-06-04 18:15:53 -0700

    Added filter logic to read queries, pass into conditions array that is executed in SQL query

--- src/services/service.js
+++ src/services/service.js
@@ -1,10 +1,34 @@
 import pool from "../scripts/db.js";
 
-const fetchProducts = async () => {
-    const sql = "SELECT * FROM products ORDER BY id";
+const fetchProducts = async (filters = {}) => {
+    const conditions = [];
+    const params = [];
+
+    if (filters.name) {
+        conditions.push("productName LIKE ?");
+        params.push(`%${filters.name}%`);
+    }
+
+    if (filters.category) {
+        conditions.push("productType = ?");
+        params.push(filters.category);
+    }
+
+    if (filters.minPrice) {
+        conditions.push("producePrice >= ?");
+        params.push(Number(filters.minPrice));
+    }
+
+    if (filters.maxPrice) {
+        conditions.push("producePrice <= ?");
+        params.push(Number(filters.maxPrice));
+    }
+
+    const where = conditions.length ? ` WHERE ${conditions.join(" AND ")}` : "";
+    const sql = `SELECT * FROM products${where} ORDER BY id`;
 
     try {
-        const [rows] = await pool.query(sql);
+        const [rows] = await pool.query(sql, params);
         return rows;
     } catch (err) {
         console.log("-- Error retrieving data from database. --");
@@ -13,12 +37,13 @@ const fetchProducts = async () => {
         return null;
     }    
 }
-const fetchProductById = async () => {
-    const sql = "SELECT * FROM products ORDER BY id = ?";
+
+const fetchProductById = async (id) => {
+    const sql = "SELECT * FROM products WHERE id = ?";
 
     try {
         const [rows] = await pool.query(sql, [id]);
-        return rows;
+        return rows[0];
     } catch (err) {
         console.log("-- Error retrieving data from database. --");
         console.log(err);
@@ -27,8 +52,8 @@ const fetchProductById = async () => {
     }    
 }
 
-export const getAllProducts = async () => {
-    return await fetchProducts();
+export const getAllProducts = async (filters = {}) => {
+    return await fetchProducts(filters);
 };
 
 export const getProductById = async (id) => {
