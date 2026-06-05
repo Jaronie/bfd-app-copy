e7d01cbfe28ab1a53c652e4cd35e3630d526dd6a
Jaron <jayisaac6706@gmail.com>
2026-06-04 21:24:58 -0700

    Working on fetch() implementation

--- public/js/load.js
+++ public/js/load.js
@@ -1,1 +1,22 @@
-// stuff that goes into load.js
+loadProducts();
+
+async function loadProducts() {
+    const url = "http://localhost:8001/api/products";
+    const config = {
+        method: "get"
+    }
+    const response = await fetch(url, config);
+    if(response.ok){
+        //converts response body to JSON etxt
+        const products = await response.json();
+
+        //make call to display product cards here
+
+    }
+}
+
+function showProducts(mons) {
+    for(let product of mons){
+        //...
+    }
+}
